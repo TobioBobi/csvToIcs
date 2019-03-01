c1e92a1564db8d0cb901b60e35fe7635550727f7
Tobias Heer <tobias.heer@fct.de>
2019-03-01 14:19:15 +0100

    start

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,22 @@ namespace csvToIcs
     {
         static void Main(string[] args)
         {
-            //test
+            var importDir = Path.Combine(Directory.GetCurrentDirectory(), "import");
+
+            if (!Directory.Exists(importDir))
+            {
+                Console.WriteLine("No import directory found! Creating it now...");
+                Directory.CreateDirectory(importDir);
+            }
+
+            var allCsvFiles = Directory.GetFiles(importDir, "*.csv");
+
+            if (allCsvFiles.Length <= 0)
+            {
+                Console.WriteLine("No csv-files in import directory found!");
+                Console.WriteLine("Press any key...");
+                Console.ReadKey();
+            }
         }
     }
 }
